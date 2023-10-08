e3da8bd77969c402ce57f536019fa291c1526fb3
Lucas Mota <lucasmota7@gmail.com>
2023-10-08 08:28:38 -0300

    Generate MetaData
    Generate Metadata para detalhes do produto

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -2,6 +2,8 @@ import { prisma } from "@/lib/db/prisma";
 import { notFound } from "next/navigation";
 import Image from "next/image";
 import PriceTag from "@/components/PriceTag";
+import { Metadata } from "next";
+import { cache } from "react";
 
 interface ProductPageProps {
   params: {
@@ -9,12 +11,36 @@ interface ProductPageProps {
   };
 }
 
-export default async function ProductPage({
-  params: { id },
-}: ProductPageProps) {
+const getProduct = cache(async (id: string) => {
   const product = await prisma.product.findUnique({ where: { id } });
 
   if (!product) notFound();
+
+  return product;
+});
+
+export async function generateMetadata({
+  params: { id },
+}: ProductPageProps): Promise<Metadata> {
+  const product = await getProduct(id);
+
+  return {
+    title: product.name + " - FlowMazon",
+    description: product.description,
+    openGraph: {
+      images: [
+        {
+          url: product.imageUrl,
+        },
+      ],
+    },
+  };
+}
+
+export default async function ProductPage({
+  params: { id },
+}: ProductPageProps) {
+  const product = await getProduct(id);
 
   return (
     <div className="flex flex-col gap-4 lg:flex-row lg:items-center">
